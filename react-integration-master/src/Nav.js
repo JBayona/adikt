f1ee874eb5b67b7826dd8c16688f3c1260f205bc
JBayona <jordavids_22@hotmail.com>
2021-01-20 11:53:42 -0600

    Adding Credentials.

--- react-integration-master/src/Nav.js
+++ react-integration-master/src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return (
         <nav className={"navbar bg-primary"}>
             <a className={"navbar-brand text-light"} href="/" style={{color: "#fff"}}>
-                Back4App
+                Adikt
             </a>
         </nav>
     );
--- react-integration-master/src/environments.js
+++ react-integration-master/src/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-    APPLICATION_ID: 'APPLICATION_ID',
-    JAVASCRIPT_KEY: 'JAVASCRIPT_KEY',
+    APPLICATION_ID: 'Fb6fdNo2q6P4OUPKByfDQghmdFk3zA9YhklKwvr9',
+    JAVASCRIPT_KEY: 'qcrBQml7iDZ9O68vjKPhzeLPLMjmaSGUbuc3zAWt',
     SERVER_URL: 'https://parseapi.back4app.com/'
 }
