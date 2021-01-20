2fdecc2e8f392523c640693e3978c17c1d02cfc1
JBayona <jordavids_22@hotmail.com>
2021-01-20 14:25:29 -0600

    Doc comments.

--- react-integration-master/src/environments.js
+++ react-integration-master/src/environments.js
@@ -1,3 +1,11 @@
+// Documentation
+/*
+Parse and Back4app
+https://docs.parseplatform.org/parse-server/guide/#usage
+https://parse-dashboard.back4app.com/apps/09db024d-c646-48a3-8ea7-8b31d42bd164/browser/Class
+https://dashboard.back4app.com/apidocs/Fb6fdNo2q6P4OUPKByfDQghmdFk3zA9YhklKwvr9?javascript#Class-custom-class
+*/
+
 module.exports = {
     APPLICATION_ID: 'Fb6fdNo2q6P4OUPKByfDQghmdFk3zA9YhklKwvr9',
     JAVASCRIPT_KEY: 'qcrBQml7iDZ9O68vjKPhzeLPLMjmaSGUbuc3zAWt',
